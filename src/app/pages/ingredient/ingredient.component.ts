dbc980a6d408dedc4afe45eb107935569f146dbf
cnaranjo <cnaranjo@altioracorp.com>
2020-03-07 21:51:02 -0500

    Se crea la pantalla de adminsitracion de ingredientes

--- src/app/pages/ingredient/ingredient.component.ts
+++ src/app/pages/ingredient/ingredient.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, IterableChangeRecord } from '@angular/core';
+import { Ingredient } from 'src/app/model/ingredient.model';
+import { NzMessageService } from 'ng-zorro-antd';
 
 @Component({
   selector: 'app-ingredient',
@@ -7,9 +9,85 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IngredientComponent implements OnInit {
 
-  constructor() { }
+  modalTitle: String;
+
+  ingredients: Array<Ingredient> = [
+    {
+      id:1,
+      name:"i1"
+    },
+    {
+      id:2,
+      name:"i2"
+    }
+  ];
+  name: String = "";
+  ingredientSelected: Ingredient = null;
+
+  isVisible = false;
+
+  constructor(
+    private message: NzMessageService
+  ) { }
 
   ngOnInit() {
+  }
+
+  loadIngredients() : void {
+    
+  }
+
+  newIngredient(): void{
+    this.modalTitle = 'Nuevo Ingrediente';
+    this.name="";
+    this.isVisible = true;
+  }
+
+  editIngredient(ingredientModel: Ingredient): void {
+    this.modalTitle = 'Editar Ingrediente';
+    this.ingredientSelected = ingredientModel;
+    this.name = ingredientModel.name;
+    this.isVisible = true;
+  }
+
+  deleteIngredient(ingredientModel: Ingredient): void {
+
+  }
+
+  handleOk($event: MouseEvent): void {
+    let exit: Boolean = true;
+    if(this.name == ""){
+      this.message.create('error', 'Por favor ingrese el nombre del ingrediente');
+      exit = false;
+    }
+    if(exit){
+      if(this.ingredientSelected == null){
+        console.log('the ingredient register will be save');
+        let newIngredient = new Ingredient();
+        newIngredient.name = this.name;
+        //this.clientService.saveClient(newIngredient).subscribe(()=>{
+          //this.loadClientList();
+          //this.message.create('success', `Cliente nuevo guardado`);
+        //})
+      }else if (this.ingredientSelected.name != this.name){
+        console.log('the ingredient register will be update');
+        this.ingredientSelected.name = this.name;
+        //this.clientService.updateClient(this.ingredientSelected).subscribe(()=>{
+          //this.loadClientList();
+          //this.message.create('success', `Cliente editado`);
+        //});
+      }
+      this.ingredientSelected =null;
+      this.isVisible = false;
+    }
+  }
+
+  handleCancel($event: MouseEvent): void {
+    this.isVisible = false;
+  }
+
+  cancel(): void{
+
   }
 
 }
